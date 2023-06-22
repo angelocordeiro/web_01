9908ad114f52cede688ac55afec4a9740aada64e
Angelo Cordeiro <anjin157@gmail.com>
2023-06-22 19:00:03 -0300

    função contato

--- projetoweb01/app_web1/urls.py
+++ projetoweb01/app_web1/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="index"),
     path('aluno_list/<int:id>', views.aluno_list,name="aluno_list"),
+    path('contato/', views.contato, name='contato'),
 ]
--- projetoweb01/app_web1/views.py
+++ projetoweb01/app_web1/views.py
@@ -10,3 +10,19 @@ def aluno_list(request, id):
     alunos = Aluno.objects.get(id=id)
     context = {'aluno': alunos}
     return render(request, 'aluno_list.html', context)
+
+from django.shortcuts import render
+
+def contato(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        mensagem = request.POST['mensagem']
+        
+        print(f'Nome: {nome}')
+        print(f'Email: {email}')
+        print(f'Mensagem: {mensagem}')
+        
+        return render(request, 'contato.html')
+
+    return render(request, 'contato.html')
