8ae8f324f962be6d857e67b21a93565847079e8d
Angelo Cordeiro <58036378+angelocordeiro@users.noreply.github.com>
2023-06-21 23:30:17 -0300

    primeiro commit

--- projetoweb01/projetoweb01/urls.py
+++ projetoweb01/projetoweb01/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from app_web1 import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    
+    path("", include("app_web1.urls")),   
 ]
